2af4a457966849a5ccc5b7f940507124edf4d275
Yurii Heivakh <yuraheyvakh@gmail.com>
2019-03-22 11:51:17 +0200

    build Movie API

---
+++ models/rental.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const rentalSchema = mongoose.Schema({
+  customer: {
+    type: new mongoose.Schema({
+      name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      },
+      isGold: {
+        type: Boolean,
+        default: false
+      },
+      phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+      }
+    }),
+    required: true
+  },
+  movie: {
+    type: new mongoose.Schema({
+      title: {
+        type: String,
+        required: true,
+        minlength: 5,
+      },
+      dailyRentalRate: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 255
+      }
+    }),
+    required: true
+  },
+  dateOut: { 
+    type: Date, 
+    required: true,
+    default: Date.now
+  },
+  dateReturned: { 
+    type: Date
+  },
+  rentalFee: { 
+    type: Number, 
+    min: 0
+  }
+})
+
+const Rental = mongoose.model('Rental', rentalSchema);
+
+function validateRental(rental) {
+  const schema = {
+    customerId: Joi.string().required(),
+    movieId: Joi.string().required()
+  };
+
+  return Joi.validate(rental, schema);
+}
+
+exports.Rental = Rental; 
+exports.validate = validateRental;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const { Genre } = require('./models/genre');
 const genres = require('./routes/genres');
 const { Movie } = require('./models/movies');
 const movies = require('./routes/movies');
+const { Rental } = require('./models/rental');
+const rentals = require('./routes/rentals');
 const bodyParser = require('body-parser');
 
 mongoose.Promise = global.Promise;
@@ -14,7 +16,7 @@ mongoose.connect('mongodb://localhost:27017/vidly');
 app.use(bodyParser.json());
 app.use('/api', genres)
 app.use('/api/movies', movies)
-
+app.use('/api/rentals', rentals)
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
