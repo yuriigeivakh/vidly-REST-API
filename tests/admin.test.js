71c5d036eda5a95d646d5455aaab0fb9d5ca4b18
Yurii Heivakh <yuraheyvakh@gmail.com>
2019-03-26 14:41:50 +0200

    add jest and write admin middleware test

---
+++ tests/admin.test.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const { User, generateAuthToken } = require('../models/user')
+
+test('user.generateAuthToken', () => {
+  const payload = { 
+    _id: new mongoose.Types.ObjectId().toHexString(), 
+    isAdmin: true
+  }
+  const user = new User(payload);
+  const token = user.generateAuthToken();
+  const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+  expect(decoded).toMatchObject(payload);
+})
