fe5331a7d6466bd269a1e1941b7a25845ed40e42
Yurii Heivakh <yuraheyvakh@gmail.com>
2019-04-02 12:04:24 +0300

    setting up test environment

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 
+
 const port = process.env.port || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Server is running');
 })
 
-
+module.exports = server;
